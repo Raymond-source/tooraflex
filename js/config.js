a08c3dc095d08758ce2f6e6c86637e1a13f48dfc
Raymond-source <70918350+Raymond-source@users.noreply.github.com>
2026-08-03 15:44:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,24 @@
 // js/config.js
 const siteConfig = {
+  // Business Info
+  businessName: "Toora Flex",
+  businessAlternateName: "TooraFlex",
   whatsappNumber: "27664575904",
   phoneNumber: "+27664575904",
   emergencyNumber: "+27812194023",
   email: "info@tooraflex.co.za",
   address: "012 Central, 367 Helen Joseph Street, Pretoria CBD, Pretoria, 0002",
-  whatsappMessage:
-    "Hello Toora Flex, I'd like more information about your packages",
+  whatsappMessage: "Hello Toora Flex, I'd like more information about your packages",
+  
+  // Social Links
   socialLinks: {
     facebook: "https://www.facebook.com/profile.php?id=61586508505155",
-    twitter: "#",
-    linkedin: "https://www.linkedin.com/company/toora-flex/?viewAsMember=true",
+    twitter: "https://twitter.com/TooraFlex",
+    linkedin: "https://www.linkedin.com/company/toora-flex",
     instagram: "#",
   },
+  
+  // Services
   services: [
     { id: "website-design", name: "Website Design" },
     { id: "web-apps", name: "Web Applications" },
@@ -26,6 +32,17 @@ const siteConfig = {
     { id: "multiple", name: "Multiple Services" },
     { id: "custom", name: "Custom Solution" },
   ],
+  
+  // SEO Settings
+  seo: {
+    siteName: "Toora Flex",
+    siteDescription: "Smart digital solutions for South African businesses. Website design, web applications, hosting, and analytics.",
+    siteUrl: "https://www.tooraflex.co.za",
+    logoUrl: "/images/logo/logo.png",
+    ogImage: "/images/og-image.jpg",
+    twitterHandle: "@TooraFlex",
+    locale: "en_ZA",
+  }
 };
 
 function getWhatsAppUrl() {
